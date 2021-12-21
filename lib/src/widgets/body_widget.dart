cb1d6d61f14afce212259d1f922a977c1c538e16
Mario Escalante Cantu <a16021553@iteshu.edu.mx>
2021-12-21 11:10:44 -0600

    Fixed scroll bug on windows platform

--- lib/src/widgets/body_widget.dart
+++ lib/src/widgets/body_widget.dart
@@ -1,12 +1,39 @@
-
-import 'package:drawer_animation/models/product_model.dart';
+import 'package:drawer_animation/src/models/product_model.dart';
 import 'package:drawer_animation/src/widgets/item_card_widget.dart';
 import 'package:drawer_animation/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 import 'categorias.dart';
 
-class BodyWidget extends StatelessWidget {
+class BodyWidget extends StatefulWidget {
+  @override
+  State<BodyWidget> createState() => _BodyWidgetState();
+}
+
+class _BodyWidgetState extends State<BodyWidget> {
+  ScrollController _scrollController = ScrollController();
+  //Offset state <-------------------------------------
+  double offset = 0.0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _scrollController = ScrollController() //keepScrollOffset: false removed
+      ..addListener(() {
+        setState(() {
+          //<-----------------------------
+          offset = _scrollController.offset;
+          // force a refresh so the app bar can be updated
+        });
+      });
+  }
+    @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -25,21 +52,23 @@ class BodyWidget extends StatelessWidget {
         Categorias(),
         Expanded(
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: KDefaultPadding),
-              child: GridView.builder(
-                itemCount: products.length,
-
-          gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, crossAxisSpacing: KDefaultPadding, mainAxisSpacing: KDefaultPadding, childAspectRatio: 0.75, ),
-          itemBuilder: (context, index) => ItemCard(
+          padding: const EdgeInsets.symmetric(horizontal: KDefaultPadding),
+          child: GridView.builder(
+            controller: _scrollController,
+            itemCount: products.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: KDefaultPadding,
+              mainAxisSpacing: KDefaultPadding,
+              childAspectRatio: 0.75,
+            ),
+            itemBuilder: (context, index) => ItemCard(
               product: products[index],
               onPress: () {},
+            ),
           ),
-        ),
-            ))
+        ))
       ],
     );
   }
 }
-
-
