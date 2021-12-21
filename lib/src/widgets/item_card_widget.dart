bc01b9cad0695627e54632efdd8ecf9b4993e098
Mario Escalante Cantu <a16021553@iteshu.edu.mx>
2021-12-21 11:11:51 -0600

    Add item widget

---
+++ lib/src/widgets/item_card_widget.dart
@@ -0,0 +1,44 @@
+import 'package:drawer_animation/utils/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:drawer_animation/src/models/product_model.dart';
+
+class ItemCard extends StatelessWidget {
+  final Product product;
+  final Function onPress;
+  const ItemCard({
+    Key? key,
+    required this.product,
+    required this.onPress,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(KDefaultPadding),
+            // height: 180,
+            // width: 180,
+            decoration: BoxDecoration(
+                color: product.color,
+                borderRadius: BorderRadius.circular(16)),
+            child: Image.asset(product.image),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: KDefaultPadding / 4),
+          child: Text(
+            product.title,
+            style: TextStyle(color: KTextColor),
+          ),
+        ),
+        Text(
+          '\$${product.price}',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
